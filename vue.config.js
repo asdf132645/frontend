750311e5e2d024729025b31e85c68eafcc91c2ae
asdf1326 <amber1326@naver.com>
2024-02-02 22:38:28 +0900

    파일 최적화

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
 
 module.exports = {
   configureWebpack: {
@@ -6,6 +8,31 @@ module.exports = {
       alias: {
         '@': path.join(__dirname, 'src/')
       }
-    }
-  }
-}
+    },
+    plugins: [
+      new PurgeCSSPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/**/*.vue'),
+          path.join(__dirname, './public/index.html'),
+        ]),
+        // 기타 설정 옵션
+      }),
+    ],
+  },
+  // Vue 컴파일러 설정
+  // Vue 3에서 productionMode 설정 추가
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        // Vue Loader 옵션에 productionMode 추가
+        options.compilerOptions = {
+          productionMode: true,
+          ...(options.compilerOptions || {}),
+        };
+        return options;
+      });
+  },
+};
+// vue-cli-service build --mode production 최적화 빌드 방법
