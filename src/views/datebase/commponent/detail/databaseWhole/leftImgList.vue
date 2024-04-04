888653cc19adbb6aad4b381d1267e2a429d08556
kaynekaynekayne <kaynekaynekayne@github.com>
2024-04-04 17:36:14 +0900

    bm 받아오기

--- src/views/datebase/commponent/detail/databaseWhole/leftImgList.vue
+++ src/views/datebase/commponent/detail/databaseWhole/leftImgList.vue
@@ -2,14 +2,29 @@
   <ul class="">
     <p>Partical Image</p>
     <li v-for="(image, index) in paImages" :key="index">
-      <img :src="getPaImageUrl(image)" alt="Partical Image">
+      <img :src="getImageUrls(image, 'particle')" alt="Partical Image" >
+      <!-- <img :src="getPaImageUrl(image)" alt="Partical Image"> -->
     </li>
   </ul>
   <ul class="">
-    <p>Partical Image</p>
-    <li v-for="(image, index) in paImages" :key="index">
-      <img :src="getPaImageUrl(image)">
+    <p>Ideal Zone</p>
+    <li v-for="(image, index) in idealZoneImages" :key="index" style="width:100px">
+      <img :src="getImageUrls(image, 'idealZone')">
     </li>
+  </ul>
+  <ul class="">
+    <div>
+      <p>Ideal stitch</p>
+      <li v-for="(image, index) in idealStitchImages" :key="index">
+        <img :src="getImageUrls(image, 'idealStitch')">
+      </li>
+    </div>
+    <div>
+      <p>Megakaryocyte</p>
+      <li v-for="(image, index) in megaImages" :key="index">
+        <img :src="getImageUrls(image, 'mega')">
+      </li>
+    </div>
   </ul>
 </template>
 
@@ -22,8 +37,12 @@ const selectItemsData = sessionStorage.getItem("selectItems");
 const selectItems = ref(selectItemsData ? JSON.parse(selectItemsData) : null);
 const store = useStore();
 const apiBaseUrl = process.env.APP_API_BASE_URL || 'http://192.168.0.131:3002';
-const paImages = ref([]);
 
+const paImages = ref([]);
+const idealZoneImages = ref([]);
+const idealStitchImages = ref([]);
+const megaImages = ref([]);
+const strArray = ['02_Particle_Image','03_Cell_Ideal_Image', '04_Cell_Ideal_Stitch_Image', '05_Mega_Image'];
 
 
 onMounted(() => {
@@ -31,26 +50,57 @@ onMounted(() => {
 })
 
 
-const getPaImageUrl = (imageName: string) => {
+const getImageUrls = (imageName: string, type: string) => {
+  let folderName;
+  switch (type) {
+    case 'particle':
+      folderName = '02_Particle_Image'
+      break;
+    case 'idealZone':
+      folderName = '03_Cell_Ideal_Image'
+      break;
+    case 'idealStitch':
+      folderName = '04_Cell_Ideal_Stitch_Image'
+      break;
+    case 'mega':
+      folderName = '05_Mega_Image'
+      break;
+    default:
+      break;
+  }
   const slotId = selectItems.value?.slotId || "";
-  console.log(sessionStorage.getItem('pbiaRootPath'))
-  const folderPath = `${sessionStorage.getItem('pbiaRootPath')}/${slotId}/03_Cell_Ideal_Image`;
+  const folderPath = `${sessionStorage.getItem('pbiaRootPath')}/${slotId}/${folderName}`;
+
   return `${apiBaseUrl}/folders?folderPath=${folderPath}/${imageName}`;
 };
 
 const getImgUrl = () => {
   const slotId = selectItems.value?.slotId || "";
   console.log(sessionStorage.getItem('pbiaRootPath'))
-  axios.get(`${apiBaseUrl}/folders?folderPath=${sessionStorage.getItem('pbiaRootPath')}/${slotId}/03_Cell_Ideal_Image`)
+
+  for (const item of strArray) {
+    axios.get(`${apiBaseUrl}/folders?folderPath=${sessionStorage.getItem('pbiaRootPath')}/${slotId}/${item}`)
       .then(response => {
-        // 서버에서 받은 응답을 처리합니다.
-        paImages.value = response.data;
+        switch (item) {
+          case '02_Particle_Image':
+            paImages.value = response.data;
+            break;
+          case '03_Cell_Ideal_Image':
+            idealZoneImages.value = response.data;
+            break;
+          case '04_Cell_Ideal_Stitch_Image':
+            idealStitchImages.value = response.data;
+            break;
+          case '05_Mega_Image':
+            megaImages.value = response.data;
+          default:
+            break;
+        }
       })
       .catch(error => {
-        // 오류 처리
         console.error('Error:', error.response.data);
       });
-
+  }
 }
 
 </script>
