2edbaf31cacfe7318e057a6f35d75fce84aa8980
coin <amber132623@gmail.com>
2024-04-08 11:01:30 +0900

    .

--- src/plugins/socketPlugin.ts
+++ src/plugins/socketPlugin.ts
@@ -9,13 +9,10 @@ declare module '@vue/runtime-core' {
 }
 const apiBaseUrl = process.env.APP_API_BASE_URL || 'http://192.168.0.131:3002';
 
-// 플러그인을 정의합니다.
 const socketPlugin: Plugin = {
     install: (app) => {
-        // 소켓 인스턴스를 생성하고 구성합니다.
         const socket: Socket = io(`${apiBaseUrl}`, { transports: ['websocket'], withCredentials: true });
 
-        // 소켓 인스턴스를 앱 컨텍스트에 추가합니다.
         app.config.globalProperties.$socket = socket;
     },
 };
