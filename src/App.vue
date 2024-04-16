8d446bcd97e7a6220ea546fe05aa4a4bcad927b8
coin <amber132623@gmail.com>
2024-04-16 16:45:55 +0900

    쉊ㅇ

--- src/App.vue
+++ src/App.vue
@@ -516,6 +516,14 @@ const cellImgGet = async (newUserId: string) => {
         const data = result.data;
         sessionStorage.setItem('pbiaRootPath', data?.pbiaRootPath);
         await store.dispatch('commonModule/setCommonInfo', {pbiaRootPath: String(data?.pbiaRootPath)});
+        await store.dispatch('dataBaseSetDataModule/setDataBaseSetData', {
+          isNsNbIntegration: data?.isNsNbIntegration ? 'Y' : 'N'
+        });
+        // 공통으로 사용되는 부분 세션스토리지 저장 새로고침시에도 가지고 있어야하는부분
+        sessionStorage.setItem('isNsNbIntegration', isNsNbIntegration.value);
+        sessionStorage.setItem('wbcPositionMargin', data?.wbcPositionMargin);
+        sessionStorage.setItem('rbcPositionMargin', data?.rbcPositionMargin);
+        sessionStorage.setItem('pltPositionMargin', data?.pltPositionMargin);
       }
     }
 
