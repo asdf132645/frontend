e9e9a865830367e45f183bab1f731fde3864dff9
henry <qooktree@gmail.com>
2024-05-20 10:18:23 +0900

    setting version 탭 작성

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -13,8 +13,9 @@ import AnalysisDatabase from "@/views/setting/analysisDatabase/index.vue";
 import LognAccount from "@/views/setting/lognAccount/index.vue";
 import Report from "@/views/setting/report/index.vue";
 import QualityCheck from "@/views/setting/qualityCheck/index.vue";
+import Version from "@/views/setting/version/index.vue";
 
-const tabs = ['Login/Account', 'Analysis/Database', 'Report', 'Quality Check'] as const;
+const tabs = ['Login/Account', 'Analysis/Database', 'Report', 'Quality Check', 'Version'] as const;
 const currentTab = ref<typeof tabs[number]>(tabs[0]);
 
 const changeTab = (tab: typeof tabs[number]) => {
@@ -22,7 +23,7 @@ const changeTab = (tab: typeof tabs[number]) => {
   sessionStorage.setItem('selectedTab', tab);
 };
 
-const components = { 'Login/Account': LognAccount, 'Analysis/Database': AnalysisDatabase, 'Report': Report, 'Quality Check': QualityCheck };
+const components = { 'Login/Account': LognAccount, 'Analysis/Database': AnalysisDatabase, 'Report': Report, 'Quality Check': QualityCheck, 'Version': Version };
 
 const storedTab = sessionStorage.getItem('selectedTab');
 if (storedTab && tabs.includes(storedTab as typeof tabs[number])) {
