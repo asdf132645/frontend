31f5b0de3d217c80f60fadd33c2026167c2549c2
coin <amber132623@gmail.com>
2024-06-17 10:19:45 +0900

    .

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 window.APP_API_BASE_URL='http://192.168.0.131:3002';
 window.PROJECT_TYPE='pb';
 window.MAIN_API='127.0.0.1'; // MAIN_API 고정
-window.PROJECT_VERSION='0.0.57v';
-window.AI_VERSION='0.0.31v';
-window.WEB_BACKEND_VERSION='0.0.11v'
-window.WEB_FRONTEND_VERSION='0.0.57v'
+window.PROJECT_VERSION='0.0.81v';
+window.AI_VERSION='';
+window.WEB_BACKEND_VERSION='0.0.28v'
+window.WEB_FRONTEND_VERSION='0.0.81v'
 window.PB_VERSION='12a'
