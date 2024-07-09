1f869313469ae94133f70f425213365fef09d4f9
henry <kaynekaynekayne@github.com>
2024-07-09 13:05:31 +0900

    0.1.21v_(FE, BE 버전 숫자 업데이트)

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 window.APP_API_BASE_URL='http://127.0.0.1:3002';
 window.PROJECT_TYPE='pb';
 window.MAIN_API='127.0.0.1'; // MAIN_API 고정
-window.PROJECT_VERSION='0.1.12v';
+window.PROJECT_VERSION='0.1.21v';
 window.AI_VERSION='';
-window.WEB_BACKEND_VERSION='0.0.41v'
-window.WEB_FRONTEND_VERSION='0.1.12v'
+window.WEB_BACKEND_VERSION='0.0.43v'
+window.WEB_FRONTEND_VERSION='0.1.21v'
 window.PB_VERSION='12a'
