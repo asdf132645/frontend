2eb279adc2c9578227453a3c1d3a02b80e79b582
coin <amber132623@gmail.com>
2024-07-09 15:12:07 +0900

    .

--- src/views/datebase/commponent/detail/classInfo/listDetail.vue
+++ src/views/datebase/commponent/detail/classInfo/listDetail.vue
@@ -641,6 +641,7 @@ watch(() => classInfoSort.value, async (newItem) => { // 오더클래스부분 �
 });
 
 const refreshClass = async (data: any) => {
+  cellMarkerIcon.value = false;
   classCompareShow.value = false;
   selectItems.value = data;
   const path = selectItems.value?.rootPath !== '' && selectItems.value?.rootPath ? selectItems.value?.rootPath : store.state.commonModule.iaRootPath;
