c2f467c26dbc0160a47ede8f487b44d6dc446ed3
coin <amber132623@gmail.com>
2024-08-06 19:00:10 +0900

    .

--- src/views/datebase/commponent/detail/rbc/rbcClass.vue
+++ src/views/datebase/commponent/detail/rbc/rbcClass.vue
@@ -641,8 +641,8 @@ const afterChange = async (newItem?: any) => {
 
   rbcInfoBeforeVal.value = rbcData.rbcInfo?.rbcClass ? rbcData.rbcInfo.rbcClass : rbcData;
   rbcInfoAfterVal.value = rbcData?.rbcInfoAfter ? rbcData.rbcInfoAfter : rbcData.rbcInfoAfter;
-  console.log(areDegreesIdentical(rbcInfoBeforeVal.value, rbcInfoAfterVal.value))
-  rbcInfoAfterVal.value = areDegreesIdentical(rbcInfoBeforeVal.value, rbcInfoAfterVal.value) ? rbcInfoBeforeVal.value : rbcInfoAfterVal.value;
+
+  // rbcInfoAfterVal.value = areDegreesIdentical(rbcInfoBeforeVal.value, rbcInfoAfterVal.value) ? rbcInfoBeforeVal.value : rbcInfoAfterVal.value;
 
 
   // Report 화면에서 RBC Classification 동기화 문제로 추가
@@ -1068,6 +1068,8 @@ const reDegree = async () => {
       }
     });
   });
+  rbcInfoAfterVal.value = areDegreesIdentical(rbcInfoBeforeVal.value, rbcInfoAfterVal.value) ? rbcInfoBeforeVal.value : rbcInfoAfterVal.value;
+  console.log(areDegreesIdentical(rbcInfoBeforeVal.value, rbcInfoAfterVal.value))
 }
 
 </script>
