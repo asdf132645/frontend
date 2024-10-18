fc73617fdd61dfef9eb6c9e38ece21dccfcf6374
henry <qooktree@gmail.com>
2024-10-18 16:14:03 +0900

    RBC total count 이상한 문제 수정

--- src/views/datebase/commponent/detail/rbc/rbcClass.vue
+++ src/views/datebase/commponent/detail/rbc/rbcClass.vue
@@ -2,7 +2,7 @@
   <!--  {{ jsonIsBool }}-->
   <div v-show="jsonIsBool" class="createdRbc"> Creating a new RBC classification ...</div>
   <div>
-    <div class="mt1 flexSpaceBetween">
+    <div class="mt1 flex-justify-between">
       <h3 class="wbcClassInfoLeft">RBC Classification</h3>
       <ul class="leftWbcInfo rbcClass">
         <li style="position: relative">
@@ -541,7 +541,7 @@ const rbcTotalAndReCount = async (pageNumber: any) => {
       }
     }
     switch (el.categoryId) {
-      case RBC_CODE_CLASS_ID.SHAPE.CATEGORY_ID:
+      case RBC_CODE_CLASS_ID.SIZE.CATEGORY_ID:
         total = el.classInfo.length;
         break;
       case RBC_CODE_CLASS_ID.CHROMIA.CATEGORY_ID:
