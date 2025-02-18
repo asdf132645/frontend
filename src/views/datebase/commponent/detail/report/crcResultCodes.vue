4bbd80734f749d5e29e9a5af312118a92923133c
henry <qooktree@gmail.com>
2025-02-18 13:39:43 +0900

    0.3.10v_(SD의학연구소 Comment 추가 시 Enter처리되어 있는 현상 제거 요청 구현 & 길병원 제외한 나머지 병원 Report 페이지 barcodeID 나오지 않는 오류 수정)

--- src/views/datebase/commponent/detail/report/crcResultCodes.vue
+++ src/views/datebase/commponent/detail/report/crcResultCodes.vue
@@ -911,12 +911,15 @@ const updateList = (newList: any[], type: string) => {
   }
 }
 const remarkallContentPush = (newList: any, list) => {
+  const seperator = siteCd.value === HOSPITAL_SITE_CD_BY_NAME['SD의학연구소'] ? '\r' : '\r\r';
   for (const el of newList) {
-    if (list.value[0].remarkAllContent.length === 0) {
-      list.value[0].remarkAllContent += convertToNewlines(el.remarkAllContent);
-    } else {
-      list.value[0].remarkAllContent += '\r\r'
-      list.value[0].remarkAllContent += convertToNewlines(el.remarkAllContent);
+    for (const el of newList) {
+      if (list.value[0].remarkAllContent.length === 0) {
+        list.value[0].remarkAllContent += convertToNewlines(el.remarkAllContent);
+      } else {
+        list.value[0].remarkAllContent += seperator;
+        list.value[0].remarkAllContent += convertToNewlines(el.remarkAllContent);
+      }
     }
   }
   // list.value[0].remarkAllContent += '\r'
